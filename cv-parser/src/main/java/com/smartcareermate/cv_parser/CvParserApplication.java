e1b296b61fa89e611bca3f9f44d12df19ff99f99
Kevin <kevin2001molinargmail.com>
2025-06-24 11:54:30 +0200

    feat(services): add cv-parser skeleton

---
+++ cv-parser/src/main/java/com/smartcareermate/cv_parser/CvParserApplication.java
@@ -0,0 +1,13 @@
+package com.smartcareermate.cv_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvParserApplication.class, args);
+	}
+
+}
