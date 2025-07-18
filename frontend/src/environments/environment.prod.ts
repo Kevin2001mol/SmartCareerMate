82b75e936845bd76f6259cc65b2e366861c1a01b
Kevin <kevin2001molinargmail.com>
2025-07-18 11:13:13 +0200

    refactor(services): updating environment.prod.ts file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
+
   keycloak: {
     url: 'https://keycloak.smartcareermate.kevinhub.dev/auth',
     realm: 'SmartCareerMate',
     clientId: 'smart-career-frontend',
   },
-  apiUrl: 'https://smartcareermate.kevinhub.dev',   // tu dominio público
+
+  apiUrl: 'https://smartcareermate.kevinhub.dev',
 };
