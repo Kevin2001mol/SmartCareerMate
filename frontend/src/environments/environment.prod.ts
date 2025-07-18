90f1c4929e1a9eadb26dcc082967c78605104d4e
Kevin <kevin2001molinargmail.com>
2025-07-18 10:26:40 +0200

    chore(services): adding correct Uris for keycloak

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+
+  /** host público del gateway  */
+  apiUrl: 'https://smartcareermate.kevinhub.dev',
+
+  /** host público de Keycloak  */
+  keycloakUrl: 'https://keycloak.smartcareermate.kevinhub.dev/auth',
+
+  /** Realm y client que creaste en KC  */
+  realm: 'SmartCareerMate',
+  clientId: 'smart-career-frontend',
 };
